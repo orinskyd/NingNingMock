847b1c25d54b8974a6bf084684ed9aa055bd516e
orinskyd <1109960@qq.com>
2026-05-28 16:20:25 +0800

    fix: add missing import android.os.Build

--- app/src/main/java/com/ningning/mock/MainActivity.kt
+++ app/src/main/java/com/ningning/mock/MainActivity.kt
@@ -9,6 +9,7 @@ import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
